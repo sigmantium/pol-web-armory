dba3ad8f41ccd719f81b3df77da4e255feb692a8
Robin Viktorsson <robi1601@student.miun.se>
2019-01-30 21:31:05 +0100

    Created some structure to the FTP-server.

--- pol-freeshard-website-skeleton/ftpserver/app.js
+++ pol-freeshard-website-skeleton/ftpserver/app.js
@@ -1,12 +1,18 @@
-const FtpSrv = require('ftp-srv');
+// Native libraries
 const fs = require('fs');
-const ftpServer = new FtpSrv('ftp://localhost:21');
 
+// External libraries
+const FtpSrv = require('ftp-srv');
+const ftpServer = new FtpSrv('ftp://localhost:21');
+const MongoClient = require('mongodb').MongoClient;
+
+// Needed files
 const neededFiles = [
     "pcs.txt",
     "pcequip.txt"
 ];
 
+// Event on the FTP server
 ftpServer.on('login', ({ connection, username, password}, resolve, reject) => {
     if (username === 'test' && password === 'test') {
         // Successful login
@@ -20,8 +26,12 @@ ftpServer.on('login', ({ connection, username, password}, resolve, reject) => {
                 console.info(`FTP server: upload successfully received - ${fileName}`); 
 
                 if (allFilesReceived()) {
-                    console.log('ALL FILES RECEIVED!');
-                    // Start the parsing process
+                    console.log('ALL FILES RECEIVED!');  
+                    // Starting the parser
+                    startParser();
+
+                    // Compare the parsed JSON with the existing collection in the MongoDB
+                    modifyCollections();
                 } else {
                     console.log('Still missing files...');
                 }
@@ -36,21 +46,45 @@ ftpServer.on('login', ({ connection, username, password}, resolve, reject) => {
     }  
 });
 
-ftpServer.on('client-error', ({ context, error }) => { 
-    console.error(`FTP server error: error interfacing with client ${context} ${error} on ftp://localhost:21 ${JSON.stringify(error)}`); 
-}); 
+// Event on the FTP server
+ftpServer.on('client-error', ({ context, error }) => console.error(`FTP server error: error interfacing with client ${context} ${error} on ftp://localhost:21 ${JSON.stringify(error)}`)); 
 
-ftpServer.listen().then(() => console.log ('Server running at ftp://localhost:21'));
+// Listener on the FTP server
+ftpServer.listen().then(() => console.log('Server running at ftp://localhost:21'));
 
+// Check if all files are received
 function allFilesReceived() {
     for (let i = 0; i < neededFiles.length; i++) {
-        if (!fileExists('./' + neededFiles[i])) 
+        if (!filesExists('./' + neededFiles[i])) 
             return false;
     }
 
     return true;
 }
 
-function fileExists(filePath) {
+// Check if all files exists
+function filesExists(filePath) {
     return fs.existsSync(filePath);
+}
+
+// Start the POL-generated data to JSON-parser
+function startParser() {
+    console.log('Starting the parser...');
+}
+
+// ModifyCollections
+function modifyCollections() {
+    
+    MongoClient.connect("mongodb://localhost:27017/armory_backend", (err, db) => {
+        
+        if (err) { 
+            // If error = collection not found, then create collection 
+        }
+
+        // Loop through parsedJSON file. Get element by character-serial
+
+        // Find and update existing element with same character-serial if exists, otherwise create new record
+
+    });
+    
 }
