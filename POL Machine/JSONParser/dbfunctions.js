90b3f7153bcf1bc264f66351e9c1acacd4b17926
Daniel Westlin <dawe1103@student.miun.se>
2019-02-20 14:51:56 +0100

    Edited MongoDB class.

--- POL Machine/JSONParser/dbfunctions.js
+++ POL Machine/JSONParser/dbfunctions.js
@@ -4,9 +4,10 @@ const FileFunctions = require('./filefunctions');
 const MongoClient = require('mongodb').MongoClient;
 
 class MongoDB {
+    
 
     // Constructor
-    MongoDB() {
+    constructor() {
         this.db = null;
     }
     
@@ -22,9 +23,9 @@ class MongoDB {
     }
 
     // Get Singleton instance
-    async getInstance() {
+    static async getInstance() {
         try {
-            return (this.db != null) ? db : await this.connect();
+            return (this.db != null) ? this.db : await this.connect();
         } catch (e) {
             return e;
         }
@@ -32,113 +33,140 @@ class MongoDB {
 
     // Update server statistics
     static async UpdateServerStats(data) {
-        const database = this.getInstance();
-        const collection = await database.collection('serverstatsservice', (err, res) => {
+        MongoClient.connect('mongodb://' + config.database.host + ':' + config.database.port, { useNewUrlParser: true }, function(err,db){
             if (err) throw err;
-        });
-
-        // Update server stats
-        await collection.insertOne({ 
-            $set: { 
-            'accounts': data.accounts,
-            'guildcount': data.guildcount,
-            'itemcount': data.itemcount,
-            'mobilecount': data.mobilecount,
-            'onlinecount': data.onlinecount,
-            'polversion': data.polversion,
-            'serverload': data.serverload,
-            'uptime': data.uptime
-            }
-        }, { upsert: true }, (err, res) => {
+            
+            var dbo = db.db("armory_backend");
+            const data = JSON.parse(fs.readFileSync('./data/server.json'));
     
-        if (err) throw err;
+            data.forEach(async (elem) => {
+                /*dbo.collection('serverstatsservice').insertOne({ $set: { 
+                    'accounts': data.accounts,
+                    'guildcount': data.guildcount,
+                    'itemcount': data.itemcount,
+                    'mobilecount': data.mobilecount,
+                    'onlinecount': data.onlinecount,
+                    'polversion': data.polversion,
+                    'serverload': data.serverload,
+                    'uptime': data.uptime
+                }}, { upsert: true }, (err, res) => {
+                
+                    if (err) throw err;
+    
+                    console.log('Updated record: ' + elem.serial);
+                });*/
+                console.log("Data:" +data);
+    
+    //            console.log('Done updating all records!');
+            });
+            db.close();
         });
     }
 
     // Update server statistics
     static async UpdateOnlinePlayers(data) {
-        const database = this.getInstance();
-        const collection = await database.collection('onlineplayerservice', (err, res) => {
+        MongoClient.connect('mongodb://' + config.database.host + ':' + config.database.port, { useNewUrlParser: true }, function(err,db){
             if (err) throw err;
-        });
-
-        data.forEach(async (elem) => {
-            await collection.findOneAndUpdate({ 'serial': elem.serial }, { $set: { 
-                'name': elem.name,
-                'gender': elem.gender,
-                'race': elem.race,
-                'title_guild': elem.title_guild,
-                'title_prefix': elem.title_prefix,
-                'title_suffix': elem.title_suffix,
-                'title_race': elem.title_race,
-                'guild': elem.guild,
-                'murderer': elem.murderer
-            }}, { upsert: true }, (err, res) => {
-
-            if (err) throw err;
-
-            console.log('Updated record: ' + elem.Serial);
+            
+            var dbo = db.db("armory_backend");
+            const data = JSON.parse(fs.readFileSync('./data/pcs_pcequip.json'));
+    
+            data.forEach(async (elem) => {
+                dbo.collection('onlineplayerservice').findOneAndUpdate({ 'serial': elem.serial }, { $set: { 
+                    'name': elem.name,
+                    'gender': elem.gender,
+                    'race': elem.race,
+                    'title_guild': elem.title_guild,
+                    'title_prefix': elem.title_prefix,
+                    'title_suffix': elem.title_suffix,
+                    'title_race': elem.title_race,
+                    'guild': elem.guild,
+                    'murderer': elem.murderer
+                }}, { upsert: true }, (err, res) => {
+                
+                    if (err) throw err;
+    
+                    console.log('Update Online Players: updated record: ' + elem.serial);
+                });
+    
+                console.log('Done updating all records!');
             });
+            db.close();
         });
     }
 
     static async UpdateGuilds(data) {
-        const database = this.getInstance();
-        const collection = await database.collection('guildsservice', (err, res) => {
+        MongoClient.connect('mongodb://' + config.database.host + ':' + config.database.port, { useNewUrlParser: true }, function(err,db){
             if (err) throw err;
-        });
-
-        data.forEach(async (elem) => {
-            await collection.findOneAndUpdate({ 'guildid': elem.guildid }, { $set: { 
-                'abbr': elem.abbr,
-                'charter': elem.charter,
-                'color': elem.color,
-                'faction': elem.faction,
-                'master': elem.master,
-                'name': elem.name,
-                'stone': elem.stone,
-                'type': elem.type,
-                'website': elem.website,
-                'members': elem.members,
-                'allies': elem.allies,
-                'enemies': elem.enemies
-            }}, { upsert: true }, (err, res) => {
-
-            if (err) throw err;
-
-            console.log('Updated record: ' + elem.Serial);
-            });
-        });
-    }
-
-    static async UploadJSON() {
-        const database = this.getInstance();
-        const collection = await database.collection(config.files.collectionname, (err, res) => {
-            if (err) throw err;
-        });
-        
-        // Get all the data
-        const data = JSON.parse(fs.readFileSync('./data/' + config.files.parsedFilename));
-        
-        // Iterate through all the data and insert it into mongo
-        data.forEach(async (elem) => {
-            await collection.findOneAndUpdate({ 'serial': elem.Serial }, { $set: { 
-                    'account': elem.Account,
-                    'name': elem.Name,
-                    'stats': elem.stats,
-                    'skills': elem.skills,
-                    'equipment': elem.equipment
-                }}, { upsert: true }, (err, res) => {
             
-                if (err) throw err;
-
-                console.log('Updated record: ' + elem.Serial);
+            var dbo = db.db("armory_backend");
+            const data = JSON.parse(fs.readFileSync('./data/pcs_pcequip.json'));
+    
+            data.forEach(async (elem) => {
+                dbo.collection('guildservices').findOneAndUpdate({ 'guildid': elem.guildid }, { $set: { 
+                    'abbr': elem.abbr,
+                    'charter': elem.charter,
+                    'color': elem.color,
+                    'faction': elem.faction,
+                    'master': elem.master,
+                    'name': elem.name,
+                    'stone': elem.stone,
+                    'type': elem.type,
+                    'website': elem.website,
+                    'members': elem.members,
+                    'allies': elem.allies,
+                    'enemies': elem.enemies
+                }}, { upsert: true }, (err, res) => {
+                
+                    if (err) throw err;
+    
+                    console.log('Updated record: ' + elem.serial);
+                });
+    
+                console.log('Done updating all records!');
             });
-
-            console.log('Done updating all records!');
+            db.close();
         });
-    }
+    }    
 
+    static async UploadJSON() { 
+        MongoClient.connect('mongodb://' + config.database.host + ':' + config.database.port, { useNewUrlParser: true }, function(err,db){
+            if (err) throw err;
+            
+            // Declaring the database with its name.
+            var dbo = db.db("armory_backend");
+
+            // get the data to put in to the DB.
+            const data = JSON.parse(fs.readFileSync('./data/pcs_pcequip.json'));
+
+            // The current data.
+            const dbData = dbo.collection("characterservices").find({});
+            console.log(data);
+            console.log("dbData: "+dbData);
+
+           
+            // Iterate through all the data and insert it into mongo
+            data.forEach(async (elem) => {
+            /*    dbo.collection("characterservices").findOneAndUpdate({ 'serial': elem.serial }, { $set: { 
+                        'account': elem.account,
+                        'name': elem.name,
+                        'stats': elem.stats,
+                        'skills': elem.skills,
+                        'equipment': elem.equipment
+                    }}, { upsert: true }, (err, res) => {
+                
+                    if (err) throw err;
+    
+                    console.log('UpdateJSON: updated record: ' + elem.serial);
+                });*/
+    
+               // console.log("Data: "+elem.serial);
+             
+            });
+            console.log('Done updating all records!');
+            db.close();
+        });
+    }    
 }
 
-module.exports = new MongoDB();
+module.exports = MongoDB;
