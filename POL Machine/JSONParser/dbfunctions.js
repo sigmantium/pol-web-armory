fc2dcb4ec19da2311e78f5be985e2af465abbacc
Robin Viktorsson <rovi1601@student.miun.se>
2019-03-06 13:12:14 +0000

    Refinements to dbfunctions.js

--- POL Machine/JSONParser/dbfunctions.js
+++ POL Machine/JSONParser/dbfunctions.js
@@ -1,6 +1,5 @@
 const config = require('./config.json');
 const fs = require('fs');
-const FileFunctions = require('./filefunctions');
 const MongoClient = require('mongodb').MongoClient;
 
 class MongoDB {
@@ -8,40 +7,16 @@ class MongoDB {
     constructor() {
         this.db = null;
     }
-    
-    // Connect to the database
-    async connect() {
-        try {
-            this.db = await MongoClient.connect('mongodb://' + config.database.host + ':' + config.database.port, { useNewUrlParser: true });
-            console.log("Connected to db");
-            return this.db;
-        } catch (e) {
-            return e;
-        }
-    }
-
-    // Get Singleton instance
-    static async getInstance() {
-        try {
-            return (this.db != null) ? this.db : await this.connect();
-        } catch (e) {
-            return e;
-        }
-    }
 
     // Update server statistics
     static async UpdateServerStats(data) {
- 
-         MongoClient.connect('mongodb://' + config.database.host + ':' + config.database.port, { useNewUrlParser: true }, async function(err,db){
+        MongoClient.connect('mongodb://' + config.database.host + ':' + config.database.port, { useNewUrlParser: true }, async (err, db) => {
 
             if (err) throw err;
-        
+
             var dbo = db.db(config.database.dbname);
-
-                //Det här går att lägga in elem.serial i en array
-
-            var d = new Date();    
-            dbo.collection('serverstatsservice').insertOne({ 
+            var d = new Date();
+            dbo.collection('serverstatsservices').insertOne({
                 'accounts': data.accounts,
                 'guildcount': data.guildcount,
                 'itemcount': data.itemcount,
@@ -50,33 +25,28 @@ class MongoDB {
                 'polversion': data.polversion,
                 'serverload': data.serverload,
                 'uptime': data.uptime,
-                'uploaded' : d
+                'uploaded': d
             }, { upsert: true }, (err, res) => {
-        
-            if (err) throw err;
-
+                if (err) throw err;
             });
-                
-                // closing db-connection.
-                db.close();
-             
-         });
+
+            db.close();
+        });
     }
 
     // Update server statistics
-    static async UpdateOnlinePlayers(data) {   
-    
-         // Insert all data into array.
-        let onlinePlayers = data.map(function(elem) { return elem.serial; });
+    static async UpdateOnlinePlayers(data) {
+        // Insert all data into array.
+        let onlinePlayers = data.map(elem => elem.serial);
         let toBeDeleted = [];
 
-        MongoClient.connect('mongodb://' + config.database.host + ':' + config.database.port, { useNewUrlParser: true }, async function(err,db){
-                if (err) throw err;
-            
-                var dbo = db.db(config.database.dbname);
+        MongoClient.connect('mongodb://' + config.database.host + ':' + config.database.port, { useNewUrlParser: true }, async (err, db) => {
+            if (err) throw err;
 
-                data.forEach((elem) => {    
-                    dbo.collection('onlineplayerservice').findOneAndUpdate({ 'serial': elem.serial }, { $set: { 
+            var dbo = db.db(config.database.dbname);
+            data.forEach((elem) => {
+                dbo.collection('playerservices').findOneAndUpdate({ 'serial': elem.serial }, {
+                    $set: {
                         'name': elem.name,
                         'gender': elem.gender,
                         'race': elem.race,
@@ -86,50 +56,46 @@ class MongoDB {
                         'title_race': elem.title_race,
                         'guild': elem.guild,
                         'murderer': elem.murderer
-                    }}, { upsert: true }, (err, res) => {
-                
-                    if (err) throw err;
-    
-                    });
-                });
-
-  
-                
-                // Get the current documents from "inventory" collection".
-                const res = await dbo.collection("onlineplayerservice").find({}).toArray();  
-                // Iterate through res array and see if res element exists in deleteArray
-                res.forEach((elem) =>{
-                    if(!onlinePlayers.includes(elem.serial) || (onlinePlayers.length == 0 || onlinePlayers === undefined)){
-                        toBeDeleted.push(elem);
                     }
+                }, {
+                    upsert: true
+                }, (err, res) => {
+                    if (err) throw err;
                 });
+            });
 
-                // Iterate through toBeDeleted and delete it from collection.
-                toBeDeleted.forEach((elem) =>{
-                    dbo.collection("onlineplayerservice").findOneAndDelete({ 'serial': elem.serial});
-                }); 
+            // Get the current documents from "inventory" collection".
+            const res = await dbo.collection("playerservices").find({}).toArray();
 
-                // closing db-connection.
-                db.close();
-            
+            // Iterate through res array and see if res element exists in deleteArray
+            res.forEach((elem) => {
+                if (!onlinePlayers.includes(elem.serial) || (onlinePlayers.length == 0 || onlinePlayers === undefined)) {
+                    toBeDeleted.push(elem);
+                }
+            });
+
+            // Iterate through toBeDeleted and delete it from collection.
+            toBeDeleted.forEach((elem) => {
+                dbo.collection("playerservices").findOneAndDelete({ 'serial': elem.serial });
+            });
+
+            // closing db-connection.
+            db.close();
         });
-
 
     }
 
-    static async UpdateGuilds(data) {   
-
-        let activeGuilds = data.map(function(elem) { return elem.guildid; });
+    static async UpdateGuilds(data) {
+        let activeGuilds = data.map(elem => elem.guildid);
         let toBeDeleted = [];
 
-        MongoClient.connect('mongodb://' + config.database.host + ':' + config.database.port, { useNewUrlParser: true }, async function(err,db){
-                if (err) throw err;
-            
-                var dbo = db.db(config.database.dbname);
+        MongoClient.connect('mongodb://' + config.database.host + ':' + config.database.port, { useNewUrlParser: true }, async (err, db) => {
+            if (err) throw err;
 
-                data.forEach(async (elem) => {
-
-                    dbo.collection('guildservice').findOneAndUpdate({ 'guildid': elem.guildid }, { $set: { 
+            var dbo = db.db(config.database.dbname);
+            data.forEach(async (elem) => {
+                dbo.collection('guildservices').findOneAndUpdate({ 'guildid': elem.guildid }, {
+                    $set: {
                         'abbr': elem.abbr,
                         'charter': elem.charter,
                         'color': elem.color,
@@ -142,64 +108,63 @@ class MongoDB {
                         'members': elem.members,
                         'allies': elem.allies,
                         'enemies': elem.enemies
-                    }}, { upsert: true }, (err, res) => {
-                    
-                        if (err) throw err;
-        
-    
-                    });
-     
-                });
-
-                // Get the current documents from "inventory" collection".
-                const res = await dbo.collection("guildservice").find({}).toArray();  
-                // Iterate through res array and see if res element exists in deleteArray
-                res.forEach((elem) =>{
-                    if(!activeGuilds.includes(elem.guildid) || (activeGuilds.length == 0 || activeGuilds === undefined)){
-                        toBeDeleted.push(elem);
                     }
+                }, { upsert: true }, (err, res) => {
+                    if (err) throw err;
                 });
+            });
 
-                // Iterate through toBeDeleted and delete it from collection.
-                toBeDeleted.forEach((elem) =>{
-                    dbo.collection("guildservice").findOneAndDelete({ 'guildid': elem.guildid});
-                }); 
+            // Get the current documents from "inventory" collection".
+            const res = await dbo.collection("guildservices").find({}).toArray();
 
+            // Iterate through res array and see if res element exists in deleteArray
+            res.forEach(elem => {
+                if (!activeGuilds.includes(elem.guildid) || (activeGuilds.length == 0 || activeGuilds === undefined)) {
+                    toBeDeleted.push(elem);
+                }
+            });
 
-                db.close();   
+            // Iterate through toBeDeleted and delete it from collection.
+            toBeDeleted.forEach((elem) => {
+                dbo.collection("guildservices").findOneAndDelete({ 'guildid': elem.guildid });
+            });
+
+            db.close();
         });
+    }
 
-    }  
-    
-    
-    static async UploadJSON() { 
-        MongoClient.connect('mongodb://' + config.database.host + ':' + config.database.port, { useNewUrlParser: true }, function(err,db){
+    static async UploadJSON() {
+        MongoClient.connect('mongodb://' + config.database.host + ':' + config.database.port, { useNewUrlParser: true }, (err, db) => {
             if (err) throw err;
-            
+
             // Declaring the database with its name.
             var dbo = db.db(config.database.dbname);
 
             // get the data to put in to the DB.
             const data = JSON.parse(fs.readFileSync('./data/pcs_pcequip.json'));
 
-           
             // Iterate through all the data and insert it into mongo
             data.forEach(async (elem) => {
-                dbo.collection("characterservices").findOneAndUpdate({ 'serial': elem.serial }, { $set: { 
+                dbo.collection("characterservices").findOneAndUpdate({
+                    'serial': elem.serial
+                }, {
+                    $set: {
                         'account': elem.account,
                         'name': elem.name,
                         'stats': elem.stats,
                         'skills': elem.skills,
                         'equipment': elem.equipment
-                    }}, { upsert: true }, (err, res) => {
-                
+                    }
+                }, {
+                    upsert: true
+                }, (err, res) => {
                     if (err) throw err;
-    
                 });
             });
+
             db.close();
         });
-    }  
+    }
 }
 
 module.exports = MongoDB;
